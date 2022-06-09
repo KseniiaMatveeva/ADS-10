d92a94236c707d97b999d2ade78d39edff74b67b
KseniiaMatveeva <kseniiamatv@gmail.com>
2022-06-09 19:23:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,10 @@
 
 std::vector<char> getPerm(const Tree& tree, int n) {
   // напишите реализацию
+  std::string restree = tree[n - 1];
+  std::vector<char> perms;
+  for (int i = 0; i < restree.length(); i++) {
+    perms.push_back(origin[i]);
+  }
+  return perms;
 }
